064e2a6c1799e8d9ddd030a7a568f0e90534e99c
root <root@master-node>
2022-03-13 21:34:26 +0530

    updated

--- main.tf
+++ main.tf
@@ -3,22 +3,22 @@ terraform {
         backend "s3" {
         bucket = "ethiraj07032022"
         key = "terraform.tfstate"
-        dynamodb_table = aws_dynamodb_table.base_dynamo_table.name
+        dynamodb_table = "tf-state-lock" 
         region =  "ap-south-1"
         }
 }
 
 
-resource "aws_dynamodb_table" "base_dynamo_table" {
-  name           = "tf-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LOCKID"
+#resource "aws_dynamodb_table" "base_dynamo_table" {
+#  name           = "tf-state-lock"
+#  billing_mode   = "PAY_PER_REQUEST"
+#  read_capacity  = 5
+#  write_capacity = 5
+#  hash_key       = "LOCKID"
 
-  attribute {
-    name = "LOCKID"
-    type = "S"
-  }
+#  attribute {
+#    name = "LOCKID"
+#    type = "S"
+#  }
 
-}
+#}
